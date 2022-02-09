de3e6137a4f8ad0e54fd71840f431efe1b541265
Esteban Lopez Ochoa <estebanlp@users.noreply.github.com>
2022-02-09 17:19:39 -0600

    adding assignment

---
+++ 04_lab/Lab4_Assignment.R
@@ -0,0 +1,30 @@
+# Lab 4 Script: Spatial Data in R
+# The University of Texas at San Antonio
+# URP-5393: Urban Planning Methods II
+
+
+#---- Instructions ----
+
+# 1. [20 points] Open the R file "Lab4_Script.R" comment each line of code with its purpose parts 1.1 through 1.3
+# 2. [80 points] Open the R file "Lab4_Assignment.R" and answer the questions
+
+
+
+#---- Q1. Search for at least one secondary data source that will inform your research project. Do the following steps:
+
+# Q1.1 Create a folder called `my_datasets` in your repository. You can do it manually or using the function `dir.create()`
+
+# Q1.2 Download the data into the `my_datasets` folder. You can do it manually or using the function `download.file()` 
+
+# Q1.3 Load the data into R creating the object `my_data_1` (change the suffix as you add 2,3 or more data sets).
+
+
+
+#---- Q2. What is the unit of analysis of your data. Use the functions `str()`, `summary()` and `View()` to describe each of the objects in your dataset. Is the unit of analysis of your data appropriate to answer your research question?
+
+
+# Q3. What are the variables of interest (outcomes) that you would like to measure in your study? Plot an histogram of the most important variable of interest. Save the plot as `plot_1.pdf` in a new folder `my_results` in your repository. 
+# Hint: use the `geom_histogram()` function. 
+# Note: if you don't have a variable of interest, select a demographic variable that would be affecting your study and use that instead.
+
+# Q4. BONUS: 20 pts. Think of the best way to show at least two of the variables/concepts your study in plot and save it as `plot_2.pdf`. What can you say about your plot?
